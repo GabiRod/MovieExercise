e135521c22d47cb58521428b68603f081b42180f
GabiRod <rodickova.gabriela@gmail.com>
2020-11-11 12:31:00 +0100

    youtube API

--- js/javascript.js
+++ js/javascript.js
@@ -16,48 +16,54 @@ var movies = [{
 }
 ];
 
+//window.onload = function () {
+// This code loads the IFrame Player API code asynchronously.
+let tag = document.createElement('script');
 
-window.onload = function () {
+tag.src = "https://www.youtube.com/iframe_api";
+let firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
+// This function creates an <iframe> (and YouTube player)
+// after the API code downloads.
 
-  // This code loads the IFrame Player API code asynchronously.
-  let tag = document.createElement('script');
-
-  tag.src = "https://www.youtube.com/iframe_api";
-  let firstScriptTag = document.getElementsByTagName('script')[0];
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-  // This function creates an <iframe> (and YouTube player)
-  // after the API code downloads.
-  let player;
-  function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-      height: '390',
-      width: '640',
-      videoId: 'youtubeId',
-      events: {
-        'onReady': onPlayerReady,
-        'onStateChange': onPlayerStateChange
-      }
-    });
-  }
-
-  //  The API will call this function when the video player is ready.
-  function onPlayerReady(event) {
-    event.target.playVideo();
-  }
-
-  //  The API calls this function when the player's state changes.
-  //  The function indicates that when playing a video (state=1),
-  //  the player should play for six seconds and then stop.
-  let done = false;
-  function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.PLAYING && !done) {
-      setTimeout(stopVideo, 6000);
-      done = true;
-    }
-  }
-  function stopVideo() {
-    player.stopVideo();
+let movieId;
+for (i = 0; i < movies.length; i++) {
+  if (document.getElementById("title").innerHTML == movies[i].title) {
+    movieId = movies[i].youtubeId;
   }
 }
+
+
+let player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '390',
+    width: '640',
+    videoId: movieId,
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+//  The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
+//  The API calls this function when the player's state changes.
+//  The function indicates that when playing a video (state=1),
+//  the player should play for six seconds and then stop.
+let done = false;
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING && !done) {
+    setTimeout(stopVideo, 6000);
+    done = true;
+  }
+}
+function stopVideo() {
+  player.stopVideo();
+}
+//}
