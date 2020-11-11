9497fb2f56c2ad1f3a771ac8da4bb63bee084024
GabiRod <rodickova.gabriela@gmail.com>
2020-11-11 11:30:11 +0100

    upload files

---
+++ js/javascript.js
@@ -0,0 +1,18 @@
+
+var movies = [{
+  "title": "Avatar",
+  "year": 2009,
+  "youtubeId": "6ziBFh3V1aM"
+},
+{
+  "title": "Alien",
+  "year": 1979,
+  "youtubeId": "LjLamj-b0I8"
+},
+{
+  "title": "Arrival",
+  "year": 2016,
+  "youtubeId": "tFMo3UJ4B4g"
+}
+];
+
