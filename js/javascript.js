3875f6769541c65b1aa3d46d45c881db5db8fb3c
GabiRod <rodickova.gabriela@gmail.com>
2020-11-16 14:02:23 +0100

    unused js

--- js/javascript.js
+++ js/javascript.js
@@ -46,11 +46,7 @@ function onYouTubeIframeAPIReady() {
   player = new YT.Player('player', {
     height: '100%',
     width: '100%',
-    videoId: movieId,
-    events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
-    }
+    videoId: movieId
   });
 }
 
@@ -98,25 +94,3 @@ for (i = 0; i < movies.length; i++) {
 
   }
 }
-
-
-//  The API will call this function when the video player is ready.
-function onPlayerReady(event) {
-  //event.target.playVideo();
-}
-
-//  The API calls this function when the player's state changes.
-//  The function indicates that when playing a video (state=1),
-//  the player should play for six seconds and then stop.
-let done = false;
-function onPlayerStateChange(event) {
-  /*if (event.data == YT.PlayerState.PLAYING && !done) {
-    //setTimeout(stopVideo, 6000);
-    //done = true;
-  }*/
-}
-function stopVideo() {
-  player.stopVideo();
-}
-    //}
-
