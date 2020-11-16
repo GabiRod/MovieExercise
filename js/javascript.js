8cce7fe7abc0649189fe72505e91ba8107c06df7
GabiRod <rodickova.gabriela@gmail.com>
2020-11-16 13:34:57 +0100

    meta and comments

--- js/javascript.js
+++ js/javascript.js
@@ -41,45 +41,6 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 // This function creates an <iframe> (and YouTube player)
 // after the API code downloads.
-
-let movieId;
-let movieName;
-let movieYear;
-
-for (i = 0; i < movies.length; i++) {
-  if (document.getElementById("title").innerHTML == movies[i].title) {
-    movieId = movies[i].youtubeId;
-    movieName = movies[i].title;
-    movieYear = movies[i].year;
-
-    fetch("https://www.omdbapi.com/?t=" + movieName + "&y=" + movieYear + "&apikey=da942d52")
-      .then(response => response.json())
-      .then(data => {
-        let currentYear = new Date();
-        let currentMovieYear = new Date(data.Released);
-        let differenceYear = Math.floor((currentYear.getTime() - currentMovieYear.getTime()) / (1000 * 3600 * 24 * 365));
-
-        document.getElementById("movieReleased").innerHTML = data.Released;
-        document.getElementById("yearCalc").innerHTML = "(" + differenceYear + " years since movie originally released)";
-        document.getElementById("movieDirector").innerHTML = data.Director;
-        document.getElementById("movieWriter").innerHTML = data.Writer;
-        document.getElementById("movieActor").innerHTML = data.Actors;
-        document.getElementById("movieGenre").innerHTML = data.Genre;
-        document.getElementById("movieDescription").innerHTML = data.Plot;
-        let ratingList = document.getElementById("ratingList");
-
-        data.Ratings.forEach(rating => {
-          let ratingItem = document.createElement('p');
-          ratingItem.appendChild(document.createElement('strong')).textContent = rating.Source;
-          ratingItem.append(" " + rating.Value);
-
-          ratingList.appendChild(ratingItem);
-        });
-      });
-
-  }
-}
-
 let player;
 function onYouTubeIframeAPIReady() {
   player = new YT.Player('player', {
@@ -92,6 +53,52 @@ function onYouTubeIframeAPIReady() {
     }
   });
 }
+
+// declare the variables of the movie 
+let movieId;
+let movieName;
+let movieYear;
+
+// loop through the movies JSON
+for (i = 0; i < movies.length; i++) {
+  if (document.getElementById("title").innerHTML == movies[i].title) {
+    movieId = movies[i].youtubeId;
+    movieName = movies[i].title;
+    movieYear = movies[i].year;
+
+    //Fetch the movie by filtering using movieName and movieYear
+    fetch("https://www.omdbapi.com/?t=" + movieName + "&y=" + movieYear + "&apikey=da942d52")
+      .then(response => response.json())
+      .then(data => {
+
+        //calculate the years since the release
+        let currentYear = new Date();
+        let currentMovieYear = new Date(data.Released);
+        let differenceYear = Math.floor((currentYear.getTime() - currentMovieYear.getTime()) / (1000 * 3600 * 24 * 365));
+
+        // append the data to the DOM
+        document.getElementById("movieReleased").innerHTML = data.Released;
+        document.getElementById("yearCalc").innerHTML = "(" + differenceYear + " years since movie originally released)";
+        document.getElementById("movieDirector").innerHTML = data.Director;
+        document.getElementById("movieWriter").innerHTML = data.Writer;
+        document.getElementById("movieActor").innerHTML = data.Actors;
+        document.getElementById("movieGenre").innerHTML = data.Genre;
+        document.getElementById("movieDescription").innerHTML = data.Plot;
+        let ratingList = document.getElementById("ratingList");
+
+        //display the ratings
+        data.Ratings.forEach(rating => {
+          let ratingItem = document.createElement('p');
+          ratingItem.appendChild(document.createElement('strong')).textContent = rating.Source;
+          ratingItem.append(" " + rating.Value);
+
+          ratingList.appendChild(ratingItem);
+        });
+      });
+
+  }
+}
+
 
 //  The API will call this function when the video player is ready.
 function onPlayerReady(event) {
